4cd2bb30d0f74e16c60e7aa22bc3c8d256788732
stencill <beat.scherrer@gmail.com>
2017-04-10 16:38:36 +0200

    deleted function for better debuggability. Initialization still not working

--- mri_reconstruction/mri_reconstruction.py
+++ mri_reconstruction/mri_reconstruction.py
@@ -12,8 +12,8 @@ import numpy as np
 import scipy as sp
 import poisson_disc
 
-from learn_dictionary import train_dictionary
-from learn_dictionary import test_dictionary
+from sklearn.decomposition import MiniBatchDictionaryLearning
+
 from create_mask import create_mask
 
 # import matlab data (johannes')
@@ -22,12 +22,14 @@ imgs = sp.io.loadmat('ismrm_ssa_imgs.mat')
 imgs = np.float64(imgs['imgs'])
 rows, cols, timesteps, persons = imgs.shape
 
-# Normalizing the images along the temporal direction
+# Normalizing the images: mean over time, plus normalize each dimension
 print("Normalizing the images")
-imgs = imgs/(2.0**16)
+imgs = imgs/255
 for i in range(imgs.shape[3]):
     temp = imgs[:,:,:,i].reshape(rows*cols,timesteps)
-    temp = temp - np.mean(temp, axis=1, keepdims=True)
+    # remove mean along time
+    temp = temp - np.mean(temp, axis=0, keepdims=True)
+    #temp = temp -np.median(temp, axis=0, keepdims=True)
     temp = temp / np.std(temp, axis=0, keepdims=True)
     temp[np.isnan(temp)] = 0
     imgs[:,:,:,i] = np.reshape(temp,imgs.shape[:-1])
@@ -53,18 +55,40 @@ reconstructions = np.real(np.fft.ifft2(np.fft.ifftshift(k_imgs), axes=(0,1)))
 # Train dictionary on fully sampled data
 print("Training...")
 train_imgs = imgs[:,:,:,0:int(0.8*persons)]
-dico, V = train_dictionary(train_imgs, n_components=50)
+
+data_train = np.transpose(train_imgs, (2,0,1,3))
+#print("data_train", data_train.shape)
+data_train = np.reshape(data_train, (timesteps, -1))
+data_train = data_train.T
+#print("Training Data shape: ", data_train.shape)
+
+# Initialize dictionary as DCT
+#init = np.zeros([timesteps,n_components])
+#for i in range(n_components):
+#    for j in range(timesteps):
+#        init[j,i] = np.cos(np.pi/timesteps*(j+0.5)*i)
+
+dico = MiniBatchDictionaryLearning(n_components=50, alpha=0.5, n_iter=100, batch_size=3, verbose=2) # n_iter = 500, batch_size = 1000
+V = dico.fit(data_train).components_
 
 # Test dictionary
 print("Testing...")
 test_imgs = reconstructions_undersampled[:,:,:,int(0.8*persons):persons]
-recs = test_dictionary(test_imgs, dico, V)
+
+data_test = np.transpose(test_imgs, (2,0,1,3))
+#data_test = np.transpose(np.abs(imgs_under[:,:,:,10], (2, 0, 1))
+data_test = np.reshape(data_test, (timesteps, -1))
+data_test = data_test.T
+code = dico.transform(data_test)
+rec = np.dot(code, V)
+rec = np.reshape(rec.T, [timesteps, rows, cols, -1])
+rec = np.transpose(rec, (1,2,0,3))
 
 # Plot various Images
-plt.figure
+plt.figure()
 plt.imshow(V,cmap='gray')
 plt.title('Dictionary')
-plt.figure
+plt.figure(figsize=(40,40))
 plt.subplot(1,5,1)
 plt.imshow(imgs[:,:,0,300], cmap='gray')
 plt.title('Reference Image')
@@ -80,3 +104,5 @@ plt.title('Reconstruction of undersampled data')
 plt.subplot(1,5,5)
 plt.imshow(recs[:,:,0,0], cmap='gray')
 plt.title('Reconstruction with Dictionary')
+
+print("dictionary representations is sparse: ", sp.sparse.issparse(code))
