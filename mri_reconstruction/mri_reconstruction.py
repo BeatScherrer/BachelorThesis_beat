aaeec3aa6af7f5cd98176877d327262038b618c8
stencill <beat.scherrer@gmail.com>
2017-04-11 12:36:59 +0200

    div

--- mri_reconstruction/mri_reconstruction.py
+++ mri_reconstruction/mri_reconstruction.py
@@ -10,8 +10,9 @@ print(__doc__)
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
-import poisson_disc
+import random
 
+from poisson_disc import Grid
 from sklearn.decomposition import MiniBatchDictionaryLearning
 
 from create_mask import create_mask
@@ -21,18 +22,25 @@ print("Importing images...")
 imgs = sp.io.loadmat('ismrm_ssa_imgs.mat')
 imgs = np.float64(imgs['imgs'])
 rows, cols, timesteps, persons = imgs.shape
+s = imgs.shape
 
 # Normalizing the images: mean over time, plus normalize each dimension
 print("Normalizing the images...")
-imgs = imgs/255
+imgs = imgs / (2**16)
+
+# normalize data
 for i in range(imgs.shape[3]):
-    temp = imgs[:,:,:,i].reshape(rows*cols,timesteps)
-    # remove mean along time
-    temp = temp - np.mean(temp, axis=0, keepdims=True)
-    #temp = temp -np.median(temp, axis=0, keepdims=True)
-    temp = temp / np.std(temp, axis=0, keepdims=True)
-    temp[np.isnan(temp)] = 0
-    imgs[:,:,:,i] = np.reshape(temp,imgs.shape[:-1])
+  temp = imgs[:,:,:,i].reshape(s[0]*s[1], s[2])
+  # remove mean along time???????????
+  #temp = temp - np.mean(temp, axis=1, keepdims=True)
+
+  temp = temp - np.median(temp,axis=0,keepdims=True)
+  temp = temp / (np.std(temp, axis=0, keepdims=True))
+  #temp[np.isnan(temp)] = 0
+  imgs[:,:,:,i] = np.reshape(temp, s[:-1])
+
+print("imgs shape:", imgs.shape)
+
 
 # Transform to k-Space
 print("Transform images to k-space...")
@@ -46,6 +54,7 @@ k_undersampled = k_imgs.copy()
 for i in range(timesteps):
     for j in range(persons):
         k_undersampled[:,:,i,j] = mask * k_imgs[:,:,i,j]
+
 
 # Reconstruction via ifft2
 print("Transform images back from k-space...")
@@ -68,7 +77,7 @@ data_train = data_train.T
 #    for j in range(timesteps):
 #        init[j,i] = np.cos(np.pi/timesteps*(j+0.5)*i)
 
-dico = MiniBatchDictionaryLearning(n_components=50, alpha=0.5, n_iter=100, batch_size=3, verbose=2) # n_iter = 500, batch_size = 1000
+dico = MiniBatchDictionaryLearning(n_components=50, alpha=0.5, n_iter=500, batch_size=1000, verbose=2) # n_iter = 500, batch_size = 1000
 V = dico.fit(data_train).components_
 
 # Test dictionary
@@ -84,25 +93,63 @@ recs = np.dot(code, V)
 recs = np.reshape(recs.T, [timesteps, rows, cols, -1])
 recs = np.transpose(recs, (1,2,0,3))
 
+code = np.reshape(code.T, [timesteps,rows,cols,-1])
+code = np.transpose(code, (1,2,0,3))
+
 # Plot various Images
 plt.figure()
-plt.imshow(V,cmap='gray')
+plt.imshow(V.T,cmap='gray')
 plt.title('Dictionary')
 plt.figure(figsize=(40,40))
-plt.subplot(1,5,1)
+plt.subplot(2,6,1)
 plt.imshow(imgs[:,:,0,300], cmap='gray')
 plt.title('Reference Image')
-plt.subplot(1,5,2)
+plt.subplot(2,6,2)
 plt.imshow(np.log(np.abs(k_imgs[:,:,0,300])),  cmap='gray')
 plt.title('Fully sampled K-space')
-plt.subplot(1,5,3)
+plt.subplot(2,6,3)
 plt.imshow(np.log(np.abs(k_undersampled[:,:,0,300])),  cmap='gray')
 plt.title('Undersampled K-space')
-plt.subplot(1,5,4)
+plt.subplot(2,6,4)
 plt.imshow(np.real(reconstructions_undersampled[:,:,0,300]),  cmap='gray')
 plt.title('Reconstruction of undersampled data')
-plt.subplot(1,5,5)
+plt.subplot(2,6,5)
 plt.imshow(recs[:,:,0,0], cmap='gray')
 plt.title('Reconstruction with Dictionary')
+plt.subplot(2,6,6)
+plt.imshow(code[:,:,0,0],cmap='gray')
+plt.title('Sparse Code')
 
-print("dictionary representations is sparse: ", sp.sparse.issparse(code))
+plt.subplot(2,6,7)
+plt.imshow(imgs[:,:,0,301], cmap='gray')
+plt.title('Reference Image')
+plt.subplot(2,6,8)
+plt.imshow(np.log(np.abs(k_imgs[:,:,0,301])),  cmap='gray')
+plt.title('Fully sampled K-space')
+plt.subplot(2,6,9)
+plt.imshow(np.log(np.abs(k_undersampled[:,:,0,301])),  cmap='gray')
+plt.title('Undersampled K-space')
+plt.subplot(2,6,10)
+plt.imshow(np.real(reconstructions_undersampled[:,:,0,301]),  cmap='gray')
+plt.title('Reconstruction of undersampled data')
+plt.subplot(2,6,11)
+plt.imshow(recs[:,:,0,1], cmap='gray')
+plt.title('Reconstruction with Dictionary')
+plt.subplot(2,6,12)
+plt.imshow(code[:,:,0,1],cmap='gray')
+plt.title('Sparse Code')
+
+print("Dictionary representations are sparse: ", sp.sparse.issparse(code))
+
+def mse(imageA, imageB):
+	err = np.sum((imageA - imageB) ** 2)
+	err /= (imageA.shape[0] * imageA.shape[1])
+
+	return err
+
+err_tot = 0
+for i in range(75):
+    for j in range(timesteps):
+        err_tot += mse(imgs[:,:,j,300+i],reconstructions_undersampled[:,:,j,300+i])-mse(imgs[:,:,j,300+i], recs[:,:,j,i])
+
+print("Total mse of testing images:", err_tot)
