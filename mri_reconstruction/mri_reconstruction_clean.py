95dd1c40a3b10e91d39efc772f7b51d327a14afd
stencill <beat.scherrer@gmail.com>
2017-06-25 23:32:26 +0200

    reconstruction with fft forcing

--- mri_reconstruction/mri_reconstruction_clean.py
+++ mri_reconstruction/mri_reconstruction_clean.py
@@ -14,39 +14,37 @@ Scikit-learn 0.19dev0
 import numpy as np
 import scipy as sp
 import random
+import time
 
 from sklearn.decomposition import MiniBatchDictionaryLearning
+from sklearn.decomposition import SparseCoder
 
 #import poisson_disc_light
 #import enhanced_grid
 
 import matplotlib.pyplot as plt
 
-def normalize(imgs):
+def normalize_data(data):
     print"normalizing..."
-    imgs = imgs / 2000
-    imgs_spat_median = np.median(imgs, axis=(0,1), keepdims=True)
-    imgs_spat_std = np.std(imgs, axis=(0,1), keepdims=True)
-    imgs_spat_std[imgs_spat_std < 1e-5] = 1e-5
-    imgs_tmp_median = np.median(imgs, axis = 2, keepdims=True)
-    
-    imgs -= imgs_spat_median
-    imgs /= imgs_spat_std
-    imgs -= imgs_tmp_median
-    
-    imgs[np.isnan(imgs)]=0
-    
-    return imgs
+    training_data = data.copy()
+    t_mean = np.mean(training_data, axis = 0, keepdims = True)
+    training_data = training_data - t_mean
+    training_data = training_data - np.mean(training_data, axis = 1, keepdims = True)
+    idxs = np.argsort( np.sum(training_data**2, axis = 1) )
+    training_data = training_data[idxs[50:], :]
+    training_data = training_data / np.std(training_data, axis = 0, keepdims = True)
+    # training_data = training_data / np.std(training_data, axis = 1, keepdims = True)
+    print 'normalization', np.mean(training_data), np.std(training_data)
+    return training_data
 
 def fft_transform(imgs):
-    print"transform to k-space..."
     k_imgs = np.fft.fft2(imgs, axes=(0,1))
     k_imgs = np.fft.fftshift(k_imgs)
     return k_imgs
 
-def inverse_fft_transform(k_imgs):
-    print"transforming back from k-space..."
-    imgs = np.real(np.fft.ifft2(np.fft.ifftshift(k_imgs), axes=(0,1)))
+def ifft_transform(k_imgs):
+    k_imgs = np.fft.ifftshift(k_imgs)
+    imgs = abs(np.fft.ifft2(k_imgs, axes=(0,1)))
     return imgs
 
 def create_masks(shape, undersampling, method, full_center=False, k=8, export_masks=False):
@@ -55,17 +53,8 @@ def create_masks(shape, undersampling, method, full_center=False, k=8, export_ma
     masks = np.zeros((shape))
     
     if method == 'uniform':
-        for t in range(shape[2]):
-            for p in range(shape[3]):
-                masks[:,:,t,p] = np.random.choice((0,1), size=(shape[:2]), p=(undersampling, 1-undersampling))
+        masks = np.random.random_sample(shape) > undersampling
         
-    if method == 'uniform_lines':
-        for t in range(shape[2]):
-            for p in range(shape[3]):
-                mask = np.random.choice((0,1), size=(shape[0]), p = (undersampling, 1-undersampling))
-                for i in range(shape[1]):
-                    masks[:,i,t,p] = mask
-    
     if method == 'gaussian_lines':
         if full_center:
             threshhold=sp.stats.norm.ppf(undersampling)
@@ -129,11 +118,9 @@ def imgs_to_data(imgs, n_features):
     return data
 
 def data_to_imgs(data, shape):
-#   expects a 4 dimensional imgs parameter, make it more generig so a 2D img can be passed
-
     data = data.T
     
-    if data.shape[0] == shape[2] * shape[0]**2:
+    if data.shape[0] == shape[0]*shape[1]:
         imgs = np.reshape(data, (shape[2], shape[0], shape[1]), order='F')
         return imgs
         
@@ -169,180 +156,174 @@ def sparsity(A):
     A = np.ravel(A)
     return 1-np.float64(np.count_nonzero(A))/len(A)
 
-def get_imgs(imgs, undersampling=0, train_amount=0.8):
-    
-#   Initialization
-    imgs_train = np.empty((imgs.shape[0], imgs.shape[1], imgs.shape[2], int(imgs.shape[3]*train_amount)))
-    imgs_test_ref = np.empty((imgs.shape[0], imgs.shape[1], imgs.shape[2], imgs.shape[3]-int(imgs.shape[3]*train_amount)))
-    
-#   Get random 80% of the images as training images
-    ind_train = np.random.choice(imgs.shape[3], int(imgs.shape[3]*train_amount), replace=False)
-    for i in range(len(ind_train)):
-        imgs_train[:,:,:,i] = imgs[:,:,:,ind_train[i]]
-        
-#   Get the remaining images as testing images and process them
-    ind_test = range(imgs.shape[3])
-    for i in range(len(ind_train)):
-        ind_test.remove(ind_train[i])
-    
-    for i in range(len(ind_test)):
-        imgs_test_ref[:,:,:,i] = imgs[:,:,:,ind_test[i]]
-    
-    return imgs_train, imgs_test_ref
+def get_imgs(imgs, train_amount=0.8):
+    perm = np.random.permutation(imgs.shape[3])
+    imgs_train = imgs[:,:,:,perm[:int(train_amount*len(perm))]]
+    imgs_test = imgs[:,:,:,perm[-int((1.0-train_amount)*len(perm)):]]
+    return imgs_train, imgs_test
 
 def initialize_dictionary(n_components, data_train):
-    if verbose:
-        print"initializing dictionary..."
     init = np.zeros((n_components, data_train.shape[1]))
     
     for k in range(int(n_components/2)):
         for n in range(data_train.shape[1]):
-            init[k,n] = np.cos((np.pi/data_train.shape[1])*n*(k+0.5))
+            init[k,n] = np.cos((np.pi/data_train.shape[1]*n*(k+0.5)))
             
     for i in range(int(n_components/2),n_components):
         init[i,:] = data_train[int(random.uniform(0,data_train.shape[0])),:]
     
     return init
 
-def export_as_mat(a,b, undersamp, n_comp, b_s, n_iter, fit_alg, transf_alg, alpha_train, alpha_test, export_info=False):
-    info = ('n_comp=' + str(n_comp) + ' batch_size=' + str(b_s) + 
+def export_plot_as_mat(a, b, undersamp, undersamp_type, n_comp, b_s, n_iter, fit_alg, transf_alg, alpha_train, alpha_test, export_info=False):
+    info = ('undersampling' + str(undersamp) + 'undersampling_type' + str(undersamp_type) + 
+            'n_comp=' + str(n_comp) + ' batch_size=' + str(b_s) + 
             ' n_iter=' + str(n_iter) + ' fit_alg=' + str(fit_alg) + 
             ' transform_alg=' + str(transf_alg) + ' alpha_train=' + 
             str(alpha_train) + ' alpha_test=' + str(alpha_test))
-    sp.io.savemat(str(a) + '_' + str(b) + '.mat', {str(a):a, str(b):b, 'info':info})
+    sp.io.savemat('psnr' + '_' + 'ncomponents' + '.mat', {str(a):a, str(b):b, 'info':info})
+    
+def learn_dictionary(imgs, n_components, alpha, fit_algorithm, n_iter, batch_size, n_jobs=1, verbose=0):
+    training_data = imgs_to_data(imgs, imgs.shape[2])
+#    training_data = normalize_data(training_data)
+    init = initialize_dictionary(n_components, training_data.copy())
+    dico = MiniBatchDictionaryLearning(n_components=n_components, alpha=alpha, fit_algorithm=fit_algorithm, dict_init=init.copy(),
+                                       n_iter=n_iter, batch_size=batch_size, verbose=1)
+    D = dico.fit(training_data).components_
+    return D, init
+
+def reconstruct_dataset(b, masks, D, p_transform_alpha, p_transform_n_nonzero_coefs, p_transform_algorithm,
+                        n_iter, n_jobs=1, verbose=0):
+
+    def reconstruct(b, masks, D, p_transform_alpha, p_transform_n_nonzero_coefs, p_transform_algorithm,
+                    n_iter, n_jobs=1, verbose=0):
+
+        Nt = masks.shape[2]
+        rows, cols = masks.shape[0:2]
+        X = np.zeros((rows,cols,Nt))
+        coder = SparseCoder(D, transform_algorithm=p_transform_algorithm, transform_n_nonzero_coefs=p_transform_n_nonzero_coefs,
+                            transform_alpha=p_transform_alpha, n_jobs=n_jobs)
+        errs = np.zeros(Nt)
+        if verbose:
+            print 'alpha_transform= ', p_transform_alpha
+        for i in range(Nt):
+            X[:,:,i] = ifft_transform(b[:,:,i])
+            print 'i:', i
+            for it in range(n_iter):
+                U = coder.transform(imgs_to_data(X, b.shape[2]))
+                DU = np.dot(U, D)
+                print 'DU:', DU.shape
+                DU = data_to_imgs(DU, X.shape[:3])
+                DU = np.squeeze(DU)
+                print 'DU imgs:', DU.shape
+                print 'X shape:', X.shape
+                # Force the measured k-space data to persist
+                for i in range(Nt):
+                    f0 = b[:,:,i]
+                    print 'f0:', f0.shape
+                    f = fft_transform(DU[:,:,i])
+                    print 'f:', f.shape
+                    f[masks[:,:,i]] = f0[masks[:,:,i]]
+                    X[:,:,i] = np.abs(ifft_transform(f))
+                fval = np.sum((X-DU)**2)
+                errs[it] = fval
+                if verbose:
+                    print it, ':', fval
+        return X, DU, U, errs
+    
+    X = np.zeros(b.shape)
+    for i in range(b.shape[3]):
+        xtmp, dutmp, utmp, errs = reconstruct(b[:,:,:,i], masks[:,:,:,i], D, p_transform_alpha, p_transform_n_nonzero_coefs, p_transform_algorithm,
+                                              n_iter, n_jobs, verbose)
+        X[:,:,:,i] = xtmp
+    return X, utmp, errs
+
+def test_run(return_info=False):   
+    # Parameters
+    n_components=60
+    undersampling = 0.5
+    train_amount = 0.8
+    
+    # Training parameters
+    batch_size = 1000
+    n_iter = 250
+    
+    # Algorithms
+    fit_algorithm = 'lars'
+    transform_algorithm = 'omp'
+    
+    # Regularization paramters
+    alpha_train = 0.2
+    
+    #Output
+    verbose = 2
+    
+    # Read
+    imgs = sp.io.loadmat('ismrm_ssa_imgs.mat')
+    imgs = np.float64(imgs['imgs'])
+    imgs /= 2000.
+    
+    # Preprocess
+    imgs_train, imgs_test_ref = get_imgs(imgs, train_amount=train_amount)
+      
+    # Train
+    D, init = learn_dictionary(imgs_train, n_components, alpha_train, fit_algorithm, n_iter,
+                         batch_size, verbose=verbose)
+    # Test
+    k_test = fft_transform(imgs_test_ref)
+    b, masks = mask_imgs(k_test, method='uniform', full_center=False, k=8, 
+                          undersampling=undersampling, n_gauss=100, variance=30, 
+                          return_masks=True)
+    imgs_test = ifft_transform(b)
+    
+    recs, utemp, errs = reconstruct_dataset(b[:,:,:,:2], masks, D, None, 5, transform_algorithm, 20, n_jobs=1, verbose=1)
+    
+    # Output chain
+    plt.figure(figsize=(10,10))
+    plt.subplot(1,2,1)
+    plt.imshow(D)
+    plt.subplot(1,2,2)
+    plt.imshow(abs(utemp[:10,:]))
+    
+    # Error vs iteration
+    plt.figure(figsize=(10,10))
+    plt.plot(errs)
+    
+    # spatial images
+    plt.figure(figsize=(10,10))
+    plt.subplot(1,3,1)
+    plt.imshow(imgs_test_ref[:,:,0,0])
+    plt.title('ground')
+    plt.subplot(1,3,2)
+    plt.imshow(imgs_test[:,:,0,0])
+    plt.title('test image')
+    plt.subplot(1,3,3)
+    plt.imshow(recs[:,:,0,0])
+    plt.title('reconstruction')
+    
+    # Slice through time
+    plt.figure(figsize=(10,10))
+    plt.subplot(1,3,1)
+    plt.imshow(imgs_test_ref[:,int(imgs_test_ref.shape[1]/2),:,0])
+    plt.title("Ground")
+    plt.xlabel('time')
+    plt.ylabel('y')
+    plt.subplot(1,3,2)
+    plt.imshow(imgs_test[:,int(imgs_test.shape[1]/2),:,0])
+    plt.title("Aliased")
+    plt.xlabel('time')
+    plt.ylabel('y')
+    plt.subplot(1,3,3)
+    plt.imshow(recs[:,int(recs.shape[1]/2),:,0])
+    plt.title("Reconstruction")
+    plt.xlabel('time')
+    plt.ylabel('y')
+    if return_info:
+        return imgs_train, imgs_test, recs, D, init
     return
-
-def plot_mat(fname):
-    return
-
-#==============================================================================
-# Variabless
-n_components = 40
-undersampling = 0.5
-train_amount = 0.8
-
-# Training Parameters
-batch_size = 1000
-n_iter = 250
-
-# Algorithms
-fit_algorithm='lars'
-transform_algorithm='lasso_lars'
-
-# Sparsity Parameter
-alpha_train = 0.2
-alpha_test = 0.5
-
-# Output amount
-verbose = 2
-
-# Plot Variables:
-resolution=10
-
-alpha_test_vec = np.linspace(0, 1, num=resolution)
-#alpha_test = np.linspace(0,1,num=resolution)
-#undersampling = np.linspace(0,1,num=resolution)
-#n_gauss_plot = np.linspace(0,100, num=resolution)
-spars=np.zeros(resolution)
-psnr = np.zeros(resolution)
 
 #==============================================================================
 # main  algorithm
 #==============================================================================
 
-# Read
-imgs = sp.io.loadmat('ismrm_ssa_imgs.mat')
-imgs = np.float64(imgs['imgs'])
-
-# Preprocess
-imgs_train, imgs_test_ref = get_imgs(imgs, train_amount=train_amount)
-
-# Select Testing and training set size    
-imgs_test_ref = imgs_test_ref[:,:,:,:]
-imgs_train = imgs_train[:,:,:,:]
-
-imgs_train = normalize(imgs_train)
-imgs_test_ref = normalize(imgs_test_ref)
-
-data_train = imgs_to_data(imgs_train, imgs.shape[2])
-
-init = initialize_dictionary(n_components, data_train)
-
-
-#alpha_test = alpha_test_vec[i]
-# Train
-dico = MiniBatchDictionaryLearning(n_components=n_components, alpha=alpha_train,
-                                   n_iter=n_iter, batch_size=batch_size, dict_init=init.copy(), verbose=verbose,
-                                   fit_algorithm=fit_algorithm, transform_algorithm=transform_algorithm)
-print"fitting data..."
-#for j in range(int(data_train.shape[0]/dico.batch_size)):
-#    data_batch = data_train[dico.batch_size*j:dico.batch_size*(j+1)]
-#    dico.partial_fit(data_batch)
-dico.fit(data_train)
-print"...done fitting."
-
-V = dico.components_
-
-# Test
-k_test = fft_transform(imgs_test_ref)
-k_mskd, masks = mask_imgs(k_test, method='uniform', full_center=True, k=8, 
-                          undersampling=undersampling, n_gauss=100, variance=30, 
-                          return_masks=True)
-imgs_test = inverse_fft_transform(k_mskd)
-imgs_test = normalize(imgs_test)
-data_test = imgs_to_data(imgs_test, imgs.shape[2])
-    
-for i in range(resolution):
-    dico.transform_alpha = alpha_test
-    print"encoding data..."
-    code = dico.transform(data_test)
-    print"...done encoding."
-    recs = np.dot(code, V)
-    recs = data_to_imgs(recs, imgs_test.shape)
-    
-    psnr[i] = peak_signal_to_noise_ratio(imgs_test_ref, recs)
-
-#code_sparsity = sparsity(code)
-#export_as_mat(alpha_train_vec, psnr, undersampling, n_components, batch_size, 
-#              n_iter, fit_algorithm, transform_algorithm, alpha_train, alpha_test, 
-#              export_info=True)
-
-
-#==============================================================================
-# Plots
-#==============================================================================
-#psnr
-plt.plot(alpha_test_vec, psnr, marker='x')
-plt.title("")
-plt.xlabel("undersampling factor")
-plt.ylabel("PSNR")
-
-#chain
-plt.figure(figsize=(10,10))
-plt.subplot(1,3,1)
-plt.imshow(imgs_test_ref[:,:,0,0])
-plt.title('ground')
-plt.subplot(1,3,2)
-plt.imshow(imgs_test[:,:,0,0])
-plt.title('test image')
-plt.subplot(1,3,3)
-plt.imshow(recs[:,:,0,0])
-plt.title('reconstruction')
-
-#slice through time
-plt.figure(figsize=(10,10))
-plt.subplot(1,3,1)
-plt.imshow(imgs_test_ref[:,int(imgs_test_ref.shape[1]/2),:,0])
-plt.title("Ground")
-plt.xlabel('time')
-plt.ylabel('y')
-plt.subplot(1,3,2)
-plt.imshow(imgs_test[:,int(imgs_test.shape[1]/2),:,0])
-plt.title("Aliased")
-plt.xlabel('time')
-plt.ylabel('y')
-plt.subplot(1,3,3)
-plt.imshow(recs[:,int(recs.shape[1]/2),:,0])
-plt.title("Reconstruction")
-plt.xlabel('time')
-plt.ylabel('y')
+if __name__=='__main__':
+    imgs_train, imgs_test, recs, D, init = test_run(return_info=True)
